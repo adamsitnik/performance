701769f3cc63f705d8291e6f603269fa0ba884fe
Adam Sitnik <adam.sitnik@gmail.com>
2019-09-03 08:15:09 +0200

    some StartsWith benchmarks

---
+++ src/benchmarks/micro/corefx/System.Runtime/Perf_StartsWith.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace System.Runtime
+{
+    public class Perf_StartsWith
+    {
+        [Params(1, 512, 200_000)]
+        public int Length;
+
+        [Params(true, false)]
+        public bool Cached;
+
+        private string left;
+
+        private string Left => Cached ? left : string.Concat(new string('a', Length), "-");
+
+        [GlobalSetup]
+        public void Setup() => left = string.Concat(new string('a', Length), "-");
+
+        [Benchmark]
+        public bool StartsWith() => Left.StartsWith("i", StringComparison.Ordinal);
+    }
+}
