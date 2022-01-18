d372f484ac8146948a0007f1547acd825f049c36
Adam Sitnik <adam.sitnik@gmail.com>
2022-01-18 19:27:26 +0100

    implement some simple benchmarks based on existing source code

--- src/benchmarks/micro/libraries/System.IO.Compression/Brotli.cs
+++ src/benchmarks/micro/libraries/System.IO.Compression/Brotli.cs
@@ -4,6 +4,7 @@
 
 using BenchmarkDotNet.Attributes;
 using MicroBenchmarks;
+using System.Collections.Generic;
 
 namespace System.IO.Compression
 {
@@ -16,7 +17,7 @@ namespace System.IO.Compression
         public override Stream CreateStream(Stream stream, CompressionLevel level) => new BrotliStream(stream, level);
 
         [Benchmark]
-        public Span<byte> Compress_WithState()
+        public Span<byte> Compress_WithState()  
         {
             using (BrotliEncoder encoder = new BrotliEncoder(GetQuality(level), Window))
             {
@@ -66,5 +67,101 @@ namespace System.IO.Compression
         
         private static int GetQuality(CompressionLevel compressLevel)
             => compressLevel == CompressionLevel.Optimal ? 11 : compressLevel == CompressionLevel.Fastest ? 1 : 0;
+    }
+
+    [BenchmarkCategory(Categories.Libraries, Categories.NoWASM)]
+    public class BrotliDefaults
+    {
+        public IEnumerable<string> UncompressedTestFileNames()
+        {
+            yield return "TestDocument.pdf"; // 199 KB small test document with repeated paragraph, PDF are common
+            yield return "alice29.txt"; // 145 KB, copy of "ALICE'S ADVENTURES IN WONDERLAND" book, an example of text file
+            yield return "sum"; // 37.3 KB, some binary content, an example of binary file
+            yield return "10x10y";
+            yield return "64x";
+            yield return "backward65536";
+            yield return "compressed_file";
+            yield return "compressed_repeated";
+            yield return "empty";
+            yield return "mapsdatazrh";
+            yield return "monkey";
+            yield return "quickfox";
+            yield return "quickfox_repeated";
+            yield return "random_org_10k.bin";
+            yield return "x";
+            yield return "ukkonooa";
+            yield return "xyzzy";
+            yield return "zeros";
+        }
+
+        [ParamsSource(nameof(UncompressedTestFileNames))]
+        public string file { get; set; }
+
+        [Params(5, 6, 7, 8, 9, 10, 11)]
+        public int quality { get; set; }
+
+        [Params(20, 21, 22, 23, 24)]
+        public int window { get; set; }
+
+        private byte[] compressedData;
+        private byte[] uncompressedData;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            string filePath = CompressedFile.GetFilePath(file);
+            uncompressedData = File.ReadAllBytes(filePath);
+            compressedData = new byte[uncompressedData.Length];
+            if (!BrotliEncoder.TryCompress(uncompressedData, compressedData, out int bytesWritten, quality, window))
+            {
+                throw new Exception("bug");
+            }
+            Array.Resize(ref compressedData, bytesWritten);
+        }
+
+        [Benchmark]
+        public Span<byte> Compress_WithState()
+        {
+            using (BrotliEncoder encoder = new BrotliEncoder(quality, window))
+            {
+                Span<byte> output = new Span<byte>(compressedData);
+                ReadOnlySpan<byte> input = uncompressedData;
+                while (!input.IsEmpty && !output.IsEmpty)
+                {
+                    encoder.Compress(input, output, out int bytesConsumed, out int written, isFinalBlock: false);
+                    input = input.Slice(bytesConsumed);
+                    output = output.Slice(written);
+                }
+                encoder.Compress(input, output, out int bytesConsumed2, out int written2, isFinalBlock: true);
+
+                return output;
+            }
+        }
+
+        [Benchmark]
+        public Span<byte> Decompress_WithState() // the level argument is not used here, but it describes how the data was compressed (in the benchmark id)
+        {
+            using (BrotliDecoder decoder = new BrotliDecoder())
+            {
+                Span<byte> output = new Span<byte>(uncompressedData);
+                ReadOnlySpan<byte> input = compressedData;
+                while (!input.IsEmpty && !output.IsEmpty)
+                {
+                    decoder.Decompress(input, output, out int bytesConsumed, out int written);
+                    input = input.Slice(bytesConsumed);
+                    output = output.Slice(written);
+                }
+
+                return output;
+            }
+        }
+
+        [Benchmark]
+        public bool Compress_WithoutState()
+            => BrotliEncoder.TryCompress(uncompressedData, compressedData, out int _, quality, window);
+
+        [Benchmark]
+        public bool Decompress_WithoutState()
+            => BrotliDecoder.TryDecompress(compressedData, uncompressedData, out int _);
     }
 }
